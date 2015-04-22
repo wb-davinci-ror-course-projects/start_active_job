9ebe2350ee84daec64e2b276101d5e009cdf7134
Wendy Brannon <wbsolutionsllc2@gmail.com>
2015-04-22 01:54:07 -0600

    *  added show as the root
    * added clickable image in the show view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# http://en.wikipedia.org/wiki/List_of_tallest_voluntarily_demolished_buildings
+Building.create(name: 'Stardust')
+Building.create(name: 'Grand Prince Hotel')
+Building.create(name: 'Hotel Grand Chancellor')
+Building.create(name: 'Landmark Tower')
+Building.create(name: 'One Meridian Plaza')
+Building.create(name: 'Farmers Bank')
+Building.create(name: 'Dunes')
+Building.create(name: 'Singer')
+Building.create(name: 'Morrison Hotel')
